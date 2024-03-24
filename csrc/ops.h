bee7d8beed9f1bd72dc08d5514ca8e6ed9e608f6
Isaac Rehg <isaacrehg@gmail.com>
2024-03-24 16:13:10 -0500

    fix: add ops definition

--- csrc/ops.h
+++ csrc/ops.h
@@ -49,6 +49,23 @@ void paged_attention_v2(
   const std::string& kv_cache_dtype,
   float kv_scale);
 
+void kvcompress_paged_attention_v2(
+  torch::Tensor& out,
+  torch::Tensor& exp_sums,
+  torch::Tensor& max_logits,
+  torch::Tensor& tmp_out,
+  torch::Tensor& query,
+  torch::Tensor& key_cache,
+  torch::Tensor& value_cache,
+  int num_kv_heads,
+  float scale,
+  torch::Tensor& block_tables,
+  torch::Tensor& context_lens,
+  int block_size,
+  int max_context_len,
+  const c10::optional<torch::Tensor>& alibi_slopes,
+  const std::string& kv_cache_dtype);
+
 void rms_norm(
   torch::Tensor& out,
   torch::Tensor& input,
