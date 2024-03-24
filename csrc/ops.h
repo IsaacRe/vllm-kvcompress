b31c6c9a49f3000b06b54cd7832842a746cc018a
Isaac Rehg <isaacrehg@gmail.com>
2024-03-24 14:20:05 -0500

    move kvcompress kernel to separate file

--- csrc/ops.h
+++ csrc/ops.h
@@ -17,6 +17,20 @@ void paged_attention_v1(
   const std::string& kv_cache_dtype,
   float kv_scale);
 
+void kvcompress_paged_attention_v1(
+  torch::Tensor& out,
+  torch::Tensor& query,
+  torch::Tensor& key_cache,
+  torch::Tensor& value_cache,
+  int num_kv_heads,
+  float scale,
+  torch::Tensor& block_tables,
+  torch::Tensor& context_lens,
+  int block_size,
+  int max_context_len,
+  const c10::optional<torch::Tensor>& alibi_slopes,
+  const std::string& kv_cache_dtype);
+
 void paged_attention_v2(
   torch::Tensor& out,
   torch::Tensor& exp_sums,
