test GQA - fix schedule_seq_evictions evicted block calculation - fix selection of first reference token during sampling - generate random digit prompt to have fixed number of tokens under the provided tokenizer

tests/conftest.py
@@ -42,7 +42,6 @@ _RANDOM_DIGIT_PROMPT_TEMPLATE = "USER: Repeat the following string of digits:\n{
 _RANDOM_DIGIT_STRINGS = [
     "3445283696391145714477754187117652483539127879511650235737670797020723087965063690307283106268121379"
 ]
-_RANDOM_DIGIT_REPEAT_LENGTH = 10
 assert len(_PIXEL_VALUES_FILES) == len(_IMAGE_FEATURES_FILES) == len(
     _IMAGE_FILES) == len(_IMAGE_PROMPTS)
 
@@ -115,17 +114,17 @@ def vllm_image_prompts(request) -> List[str]:
 
 
 @pytest.fixture()
-def random_digit_prompts() -> List[str]:
+def random_digit_prompts(repeat_len: int = 10) -> List[str]:
     return [
-        _RANDOM_DIGIT_PROMPT_TEMPLATE.format(random_digits, random_digits[:_RANDOM_DIGIT_REPEAT_LENGTH])
+        _RANDOM_DIGIT_PROMPT_TEMPLATE.format(random_digits, random_digits[:repeat_len])
         for random_digits in _RANDOM_DIGIT_STRINGS
     ]
 
 
 @pytest.fixture()
-def random_digit_responses() -> List[str]:
+def random_digit_responses(repeat_len: int = 10) -> List[str]:
     return [
-        random_digits[_RANDOM_DIGIT_REPEAT_LENGTH:]
+        random_digits[repeat_len:]
         for random_digits in _RANDOM_DIGIT_STRINGS
     ]
 
@@ -369,15 +368,17 @@ class VllmRunner:
 
     def generate_w_logprobs(
         self,
-        prompts: List[str],
         sampling_params: SamplingParams,
+        prompts: Optional[List[str]] = None,
+        prompt_token_ids: Optional[List[List[int]]] = None,
         reference_completions: Optional[List[str]] = None,
         reference_token_ids: Optional[List[List[int]]] = None,
     ) -> List[Tuple[List[int], str]]:
         assert sampling_params.logprobs is not None
 
-        req_outputs = self.model.generate(prompts,
-                                          sampling_params=sampling_params,
+        req_outputs = self.model.generate(sampling_params=sampling_params,
+                                          prompts=prompts,
+                                          prompt_token_ids=prompt_token_ids,
                                           reference_completions=reference_completions,
                                           reference_token_ids=reference_token_ids)
         outputs = []
@@ -406,16 +407,19 @@ class VllmRunner:
 
     def generate_greedy_logprobs(
         self,
-        prompts: List[str],
         max_tokens: int,
         num_logprobs: int,
+        prompts: Optional[List[str]] = None,
+        prompt_token_ids: Optional[List[List[int]]] = None,
         reference_completions: Optional[List[str]] = None,
         reference_token_ids: Optional[List[List[int]]] = None,
     ) -> List[Tuple[List[int], str]]:
         greedy_logprobs_params = SamplingParams(temperature=0.0,
                                                 max_tokens=max_tokens,
                                                 logprobs=num_logprobs)
-        outputs = self.generate_w_logprobs(prompts, greedy_logprobs_params,
+        outputs = self.generate_w_logprobs(greedy_logprobs_params,
+                                           prompts=prompts,
+                                           prompt_token_ids=prompt_token_ids,
                                            reference_completions=reference_completions,
                                            reference_token_ids=reference_token_ids)
 
@@ -447,7 +451,7 @@ def vllm_runner():
 
 @pytest.fixture()
 def random_digit_generator():
-    def random_digit_gen(num_digits, random_seed, n_seqs: int = 1):
+    def random_digit_gen(num_digits, random_seed, n_seqs: int = 1, repeat_len: int = 10):
         prompts = []
         completions = []
         np.random.seed(random_seed)
@@ -458,10 +462,43 @@ def random_digit_generator():
                 digits_string += str(d)
             prompts.append(
                 _RANDOM_DIGIT_PROMPT_TEMPLATE.format(
-                    digits_string, digits_string[:_RANDOM_DIGIT_REPEAT_LENGTH]
+                    digits_string, digits_string[:repeat_len]
                 )
             )
-            completions.append(digits_string[_RANDOM_DIGIT_REPEAT_LENGTH:])
+            completions.append(digits_string[repeat_len:])
+        return (prompts, completions)
+    return random_digit_gen
+
+
+@pytest.fixture()
+def tokenizer_dependent_random_digit_generator():
+    def random_digit_gen(tokenizer, num_tokens, random_seed, n_seqs: int = 1, repeat_len: int = 10):
+        prompts = []
+        completions = []
+        digits_per_tok_estimate = 6  # conservative estimate
+        np.random.seed(random_seed)
+        for _ in range(n_seqs):
+            digits = np.random.randint(10, size=(num_tokens * digits_per_tok_estimate,))
+            digits_string = ""
+            for d in digits:
+                digits_string += str(d)
+
+            tokens = tokenizer.encode(digits_string, add_special_tokens=False)
+            input_tokens = tokens[:num_tokens]
+            repeat_tokens = tokens[:repeat_len]
+            response_tokens = tokens[repeat_len:num_tokens]
+            assert len(tokens) >= num_tokens, "not enough tokens generated"
+            input_tokens_string = tokenizer.decode(input_tokens)
+            repeat_tokens_string = tokenizer.decode(repeat_tokens)
+            response_tokens_string = tokenizer.decode(response_tokens)
+            full_input_string = _RANDOM_DIGIT_PROMPT_TEMPLATE.format(
+                input_tokens_string, repeat_tokens_string
+            )
+            input_tokens = tokenizer.encode(full_input_string)
+            response_tokens = tokenizer.encode(response_tokens_string, add_special_tokens=False)
+
+            prompts.append(input_tokens)
+            completions.append(response_tokens)
         return (prompts, completions)
     return random_digit_gen
 

vllm/kvcompress/scheduler.py
@@ -109,16 +109,27 @@ class CompressionScheduler:
         if compression_rate is None:
             compression_rate = self.config.target_compression_rate
         if max_cache_tokens is None:
-            max_cache_tokens = self.config.max_cache_tokens
+            max_cache_tokens = (
+                self.config.max_cache_tokens + self.block_size - 1
+            ) // self.block_size * self.block_size
 
         if compression_rate < 1.0 and max_cache_tokens > 0:
             raise RuntimeError("both compression_rate and max_cache_tokens "
                                "specified during compression")
 
+        total_kv_heads = self.config.num_layers * self.config.num_kv_heads
+
+        max_cache_kv = (
+            self.config.protected_window_size * total_kv_heads
+        )
+        evict_kv_count = max(
+            0,
+            self.block_manager.get_sequence_kv_count(seq) - max_cache_kv,
+        )
         if max_cache_tokens > 0:
             # Evict by max number of KV per sequence.
             max_cache_kv = (
-                max_cache_tokens * self.config.num_layers * self.config.num_kv_heads
+                max_cache_tokens * total_kv_heads
             )
             evict_kv_count = max(
                 0,
@@ -126,35 +137,27 @@ class CompressionScheduler:
             )
         else:
             # Evict by target compression rate.
-            # Total KV count should not go below protected_window_size * num_kv_heads
-            compressible_token_count = (
-                seq.data.get_len() - self.config.protected_window_size
+            # Total KV count should not go below protected_window_size * num_kv_heads.
+            # Need to round up to next full block as remaining tokens in block cannot be
+            # evicted without evicting any protected tokens in the block as well.
+            protected_tokens = (
+                (self.config.protected_window_size + self.block_size - 1) // self.block_size
+                * self.block_size
             )
+            compressible_token_count = (seq.data.get_len() - protected_tokens)
             if compressible_token_count <= 0:
                 return 0, 0
             # Total count of KVs in compressible range if this sequence had never
             # been compressed
-            compressible_kv_count = (
-                compressible_token_count
-                * self.config.num_layers * self.config.num_kv_heads
-            )
-            # Actual count of KVs in compressible range TODO
-            compressed_kv_count = (
-                self.block_manager.get_sequence_kv_count(seq)
-                - self.config.protected_window_size * self.config.num_layers
-                * self.config.num_kv_heads
-            )
-            protected_blocks = (
-                (self.config.protected_window_size + self.block_size - 1) // self.block_size
-                * self.config.num_layers * self.config.num_kv_heads
-            )
+            compressible_kv_count = compressible_token_count * total_kv_heads
+            # Actual count of KVs currently in cache within compressible range
+            compressed_kv_count = self.block_manager.get_sequence_kv_count(seq)
+            protected_kv = protected_tokens * total_kv_heads
             # Target count of KVs in compressible range that will yield the
             # desired compression rate
-            target_kv_count = max(
-                math.ceil(compressible_kv_count * compression_rate),
-                protected_blocks * self.block_size,
+            target_kv_count = (
+                math.ceil(compressible_kv_count * compression_rate) + protected_kv
             )
-            # target_kv_count = math.ceil(compressible_kv_count * compression_rate)
 
             evict_kv_count = max(0, compressed_kv_count - target_kv_count)
 
@@ -164,7 +167,7 @@ class CompressionScheduler:
         if self.config.even_layer_evict:
             evict_block_count = ((evict_block_count // self.config.num_layers)
                                  * self.config.num_layers)
-            
+
         assert evict_block_count <= max(
             self.block_manager.get_sequence_block_count(seq)
             - (self.config.protected_window_size + self.block_size - 1) // self.block_size * self.config.num_kv_heads * self.config.num_layers,
@@ -377,7 +380,7 @@ class CompressionScheduler:
         #     >= evicted_kv_count[...,None]
         # )
         self.total_evicted += evicted_kv_count.sum().item()
-        print(f"TOTAL EVICTED:\nKVs: {self.total_evicted}\nTokens: {self.total_evicted / 32 / 32}")
+        print(f"TOTAL EVICTED:\nKVs: {self.total_evicted}\nTokens: {self.total_evicted / self.config.num_kv_heads / self.config.num_layers}")
         evicted_block_count = (evicted_kv_count + self.block_size - 1) // self.block_size
         protected_blocks = (self.config.protected_window_size + self.block_size - 1) // self.block_size
         assert (context_lens.transpose(0, 1) >= torch.minimum(context_lens.transpose(0, 1), torch.tensor(self.config.protected_window_size))).all()
@@ -495,27 +498,27 @@ class CompressionScheduler:
         removed = ((pos_ > last_token_positions[0] - 50) & (pos_ <= last_token_positions[0])).sum()
         print(f"Removing metadata for {removed} KVs in protected range")
 
-        for seq in seqs_to_compress:
-            self.block_manager.validate_protected_positions(seq)
-            break
+        # for seq in seqs_to_compress:
+        #     self.block_manager.validate_protected_positions(seq)
+        #     break
         self.compression_metrics.remove_metadata(freed_blocks)
-        for i, seq in enumerate(seqs_to_compress):
-            self.block_manager.validate_protected_positions(seq, cache_moves, local_idx=i)
-            print(f"STEP 3 (pre-schedule): {self.block_manager.kv_metrics.token_positions.flatten()[9602]=}")
-            break
+        # for i, seq in enumerate(seqs_to_compress):
+        #     self.block_manager.validate_protected_positions(seq, cache_moves, local_idx=i)
+        #     print(f"STEP 3 (pre-schedule): {self.block_manager.kv_metrics.token_positions.flatten()[9602]=}")
+        #     break
 
         # Failing after execute_cache_moves: seq=0 layer=2 head=21
-        flat_pos = self.block_manager.kv_metrics.token_positions.flatten()
-        count = cache_moves.count[0,2,21]
-        print(f"{count=}")
-        offset = cache_moves.offsets[0,2,21]
-        indices = cache_moves.index[offset:offset+count]
-        print(f"{context_lens[2,0,21]}")
-        print(f"{evicted_block_count[0,2,21]}")
-        pos_src = flat_pos[indices[:,1]]
-        pos_dst = flat_pos[indices[:,0]]
-        print(f"{pos_src=}")
-        print(f"{pos_dst=}")
+        # flat_pos = self.block_manager.kv_metrics.token_positions.flatten()
+        # count = cache_moves.count[0,2,21]
+        # print(f"{count=}")
+        # offset = cache_moves.offsets[0,2,21]
+        # indices = cache_moves.index[offset:offset+count]
+        # print(f"{context_lens[2,0,21]}")
+        # print(f"{evicted_block_count[0,2,21]}")
+        # pos_src = flat_pos[indices[:,1]]
+        # pos_dst = flat_pos[indices[:,0]]
+        # print(f"{pos_src=}")
+        # print(f"{pos_dst=}")
         # End Benchmark - 5
         # BENCHMARKER.end_range("_schedule_compression - 5")
 