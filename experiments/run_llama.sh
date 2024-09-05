e66945c4e070654e62092f74fbe7548106f2244c
Isaac Rehg <isaacrehg@gmail.com>
2024-09-05 14:59:33 -0500

    update run scripts

--- experiments/run_llama.sh
+++ experiments/run_llama.sh
@@ -1,9 +1,21 @@
+# w32, continual-compression, no bias, by compression-rate
+for metric_aggregation in 'L2-sum';
+do
+	for dataset in $(cat datasets.txt);
+	do
+		for compression_rate in 2 4 8 16 32 64;
+		do
+			python3 run_longbench.py --protected-window-size 32 --metric-collection-buffer-size 0 --prefill-metric-collection-window-size 32 --kv-head-bias-weight 0 --dataset $dataset --compression-rate $compression_rate --metric-aggregation $metric_aggregation --continual-compression --model llama3
+		done
+		python3 run_longbench.py --protected-window-size 32 --metric-collection-buffer-size 0 --prefill-metric-collection-window-size 32 --kv-head-bias-weight 0 --dataset $dataset --max-cache-tokens -1 --metric-aggregation $metric_aggregation --continual-compression --model llama3
+	done
+done
 # w32, continual-compression, no bias
 for metric_aggregation in 'L2-sum';
 do
 	for dataset in $(cat datasets.txt);
 	do
-		for max_cache_tokens in 1008 496 240 112;
+		for max_cache_tokens in 1024 1008 512 496 256 240 128 112;
 		do
 			python3 run_longbench.py --protected-window-size 32 --metric-collection-buffer-size 0 --prefill-metric-collection-window-size 32 --kv-head-bias-weight 0 --dataset $dataset --max-cache-tokens $max_cache_tokens --metric-aggregation $metric_aggregation --continual-compression --model llama3
 		done
@@ -14,7 +26,7 @@ done
 # do
 # 	for dataset in $(cat datasets.txt | grep qmsum -A 50);
 # 	do
-# 		for max_cache_tokens in 1008 496 240 112;
+# 		for max_cache_tokens in 1024 1008 512 496 256 240 128 112;
 # 		do
 # 			python3 run_longbench.py --protected-window-size 32 --metric-collection-buffer-size 0 --prefill-metric-collection-window-size 32 --kv-head-bias-weight 1 --dataset $dataset --max-cache-tokens $max_cache_tokens --metric-aggregation $metric_aggregation
 # 		done
@@ -25,21 +37,9 @@ for metric_aggregation in 'L2-sum';
 do
 	for dataset in $(cat datasets.txt);
 	do
-		for max_cache_tokens in 1008 496 240 112;
+		for max_cache_tokens in 1024 1008 512 496 256 240 128 112;
 		do
 			python3 run_longbench.py --protected-window-size 32 --metric-collection-buffer-size 0 --prefill-metric-collection-window-size 32 --kv-head-bias-weight 0 --dataset $dataset --max-cache-tokens $max_cache_tokens --metric-aggregation $metric_aggregation --model llama3
-		done
-	done
-done
-# w32, continual-compression, no bias, by compression-rate
-for metric_aggregation in 'L2-sum';
-do
-	for dataset in $(cat datasets.txt);
-	do
-		python3 run_longbench.py --protected-window-size 32 --metric-collection-buffer-size 0 --prefill-metric-collection-window-size 32 --kv-head-bias-weight 0 --dataset $dataset --max-cache-tokens -1 --metric-aggregation $metric_aggregation --continual-compression --model llama3
-		for compression_rate in 2 4 8 16 32 64;
-		do
-			python3 run_longbench.py --protected-window-size 32 --metric-collection-buffer-size 0 --prefill-metric-collection-window-size 32 --kv-head-bias-weight 0 --dataset $dataset --compression-rate $compression_rate --metric-aggregation $metric_aggregation --continual-compression --model llama3
 		done
 	done
 done
