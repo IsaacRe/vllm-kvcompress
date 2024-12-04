chunked-prefill batch fixes: - fix seq indexing of batched new block meta insertion - attention metadata fixes - pass correct chunk length for batched decoding - log logprobs, add checkpointing

tests/kvcompress/test_random_digit_repeat.py
@@ -18,6 +18,8 @@ MODEL_KV_HEAD_BIAS = {
     "mistralai/Mistral-7B-Instruct-v0.2": "./kv_head_bias_mistral.npz",
 }
 
+CHECK_LAYERS = [0, 31]
+
 
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("dtype", ["half"])
@@ -67,10 +69,264 @@ def test_no_compression(
 @pytest.mark.parametrize("num_digits", [100])
 @pytest.mark.parametrize("model", MODELS)
 @pytest.mark.parametrize("dtype", ["half"])
-@pytest.mark.parametrize("chunk_size", [32])  # -1 32
+@pytest.mark.parametrize("chunk_size", [-1])  # -1 32
 # NOTE: Increasing this in this suite will fail CI because we currently cannot
 # reset distributed env properly. Use a value > 1 just when you test.
 def test_parity_with_simulated_compression(
+    vllm_runner,
+    tokenizer_dependent_random_digit_generator,
+    checkpointer,
+    random_seed: int,
+    num_digits: int,
+    model: str,
+    dtype: str,
+    chunk_size: int,
+) -> None:
+    """Checks for alignment between the vLLM integration and the experimental implementation
+    that simulates eviction within a single forward pass.
+    """
+    checkpoint_cfg = RandomDigitCheckpointConfig(
+        model_name=model,
+        max_cache_tokens=64,
+        protected_window_size=32,
+        metric_collection_buffer_size=10,
+        num_digits=num_digits,
+        control_layers=[0, 1],
+    )
+
+    # Don't checkpoint during profiling
+    checkpointer.do_save = checkpointer.do_validate = False
+    checkpointer.set_condition(checkpoint_layer=lambda _: False)
+    vllm_model = vllm_runner(
+        model,
+        dtype=dtype,
+        enforce_eager=True,
+        enable_kvcompress=True,
+        enable_chunked_prefill=True,
+        # max_cache_tokens=checkpoint_cfg.max_cache_tokens,
+        # target_compression_rate=0.1,
+        block_size=16,
+        # protected_window_size=checkpoint_cfg.protected_window_size,
+        # metric_collection_buffer_size=checkpoint_cfg.metric_collection_buffer_size,
+        even_layer_evict=False, #True,
+        control_layers=[], #checkpoint_cfg.control_layers,
+        # save_checkpoint_dir='./checkpoint',
+        kv_head_bias_path=MODEL_KV_HEAD_BIAS[model],
+        kv_head_bias_weight=50000,
+        new_token_limit=1,
+        compression_interval=1,
+        gpu_memory_utilization=0.65,
+        max_num_batched_tokens=chunk_size if chunk_size > 0 else None, # TODO test separately
+        max_num_seqs=min(256, chunk_size) if chunk_size > 0 else 256,
+    )
+    checkpointer.set_config(checkpoint_cfg)
+    checkpointer.do_save = False
+    checkpointer.do_validate = False
+    checkpointer.override_output = False
+    checkpointer.base_dir = './checkpoints'
+    checkpointer.tolerance = 1e-4
+    checkpointer.set_condition(checkpoint_layer=lambda x: x in CHECK_LAYERS)
+    checkpointer.enable_for_prefix("flash_prefix_decode", "flash_prefix_cache", "token_", "flash_attn__")
+
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    input_token_ids, reference_token_ids = tokenizer_dependent_random_digit_generator(
+        tokenizer, num_digits, random_seed, n_seqs=10, repeat_len=10)
+
+    if "Llama-2" in model or "Mistral" in model:
+        # Llama-2 tokenizer adds an empty "" token before digits
+        reference_token_ids = [ref_tokens[1:] for ref_tokens in reference_token_ids]
+
+    # total_tokens = [
+    #     len(tokenizer.encode(prompt)) + len(completion_token_ids)
+    #     for prompt, completion_token_ids in zip(random_digit_prompts, reference_token_ids)
+    # ]
+    max_tokens = max(len(response) for response in reference_token_ids)
+
+    checkpointer.checkpoint('input_token_ids', torch.tensor(input_token_ids[0]))
+    checkpointer.checkpoint('reference_token_ids', torch.tensor(reference_token_ids[0]))
+
+    print(tokenizer.decode(input_token_ids[0]))
+    print(tokenizer.decode(reference_token_ids[0]))
+
+    topk_ll = 0
+    n_sample = 10
+    vllm_outputs = vllm_model.generate_greedy_logprobs(
+        10,
+        topk_ll,
+        prompt_token_ids=deepcopy(input_token_ids), #[deepcopy(input_token_ids[0]) for _ in range(n_sample)],
+        reference_token_ids=deepcopy(reference_token_ids), #[deepcopy(reference_token_ids)[0] for _ in range(n_sample)],
+        max_cache_tokens=checkpoint_cfg.max_cache_tokens,
+        # target_compression_rate=0.1,
+        protected_window_size=checkpoint_cfg.protected_window_size,
+        metric_collection_buffer_size=checkpoint_cfg.metric_collection_buffer_size,
+        compress_once=False,
+    )
+    del vllm_model
+    print(f"TOTAL TOKENS = {sum([len(i) + len(r) for i, r in zip(input_token_ids, reference_token_ids)])}")
+
+    def is_correct(logprobs, index):
+        max_i = None
+        max_logprob = -float('inf')
+        for i in logprobs:
+            logprob = logprobs[i].logprob
+            if logprob > max_logprob:
+                max_logprob = logprob
+                max_i = i
+        return max_i == index
+
+    for i, (ref_token_ids, (_, output, logprobs)) in enumerate(
+        zip(reference_token_ids, vllm_outputs)
+    ):
+        # assert len(logprobs) == len(ref_token_ids)
+        for i, (d, t) in enumerate(zip(logprobs, ref_token_ids)):
+            if t not in d:
+                raise Exception(f"{i}/{len(logprobs)} fail ({t} not in {d})")
+        nll = [-d[t].logprob for d, t in zip(logprobs, ref_token_ids)]
+        ppl = float("%.4f" % np.exp(sum(nll) / len(nll)))
+        acc = float("%.2f" % (sum([is_correct(d, t) for d, t in zip(logprobs, ref_token_ids)]) / len(logprobs) * 100.))
+        print(f"{acc=}, {ppl=}")
+        assert ppl < 1.1
+        assert acc > 99
+        # vllm_output = output[:len(reference_completion)]
+        # assert reference_completion == vllm_output, (
+        #     f"Test{i}:\nReference: {reference_completion!r}\nvLLM: {vllm_output!r}")
+
+
+@pytest.mark.parametrize("random_seed", [1])
+@pytest.mark.parametrize("num_digits", [100])
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("dtype", ["half"])
+@pytest.mark.parametrize("chunk_size", [32])  # -1 32
+# NOTE: Increasing this in this suite will fail CI because we currently cannot
+# reset distributed env properly. Use a value > 1 just when you test.
+def test_chunk_size_max_batch(
+    vllm_runner,
+    tokenizer_dependent_random_digit_generator,
+    checkpointer,
+    random_seed: int,
+    num_digits: int,
+    model: str,
+    dtype: str,
+    chunk_size: int,
+) -> None:
+    """Checks for alignment between the vLLM integration and the experimental implementation
+    that simulates eviction within a single forward pass.
+    """
+    checkpoint_cfg = RandomDigitCheckpointConfig(
+        model_name=model,
+        max_cache_tokens=80,
+        protected_window_size=32,
+        metric_collection_buffer_size=10,
+        num_digits=num_digits,
+        control_layers=[0, 1],
+    )
+
+    # Don't checkpoint during profiling
+    checkpointer.do_save = checkpointer.do_validate = False
+    checkpointer.set_condition(checkpoint_layer=lambda _: False)
+    vllm_model = vllm_runner(
+        model,
+        dtype=dtype,
+        enforce_eager=True,
+        enable_kvcompress=True,
+        enable_chunked_prefill=True,
+        # max_cache_tokens=checkpoint_cfg.max_cache_tokens,
+        # target_compression_rate=0.1,
+        block_size=16,
+        # protected_window_size=checkpoint_cfg.protected_window_size,
+        # metric_collection_buffer_size=checkpoint_cfg.metric_collection_buffer_size,
+        even_layer_evict=False, #True,
+        control_layers=[], #checkpoint_cfg.control_layers,
+        # save_checkpoint_dir='./checkpoint',
+        kv_head_bias_path=MODEL_KV_HEAD_BIAS[model],
+        kv_head_bias_weight=50000,
+        new_token_limit=1,
+        compression_interval=1,
+        gpu_memory_utilization=0.65,
+        max_num_batched_tokens=chunk_size if chunk_size > 0 else None, # TODO test separately
+        max_num_seqs=min(256, chunk_size) if chunk_size > 0 else 256,
+    )
+    checkpointer.set_config(checkpoint_cfg)
+    checkpointer.do_save = False
+    checkpointer.do_validate = False
+    checkpointer.override_output = False
+    checkpointer.base_dir = './checkpoints'
+    checkpointer.tolerance = 1e-4
+    checkpointer.set_condition(checkpoint_layer=lambda x: x in CHECK_LAYERS)
+    checkpointer.enable_for_prefix("flash_prefix_decode", "flash_prefix_cache", "token_", "flash_attn__")
+
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    input_token_ids, reference_token_ids = tokenizer_dependent_random_digit_generator(
+        tokenizer, num_digits, random_seed, n_seqs=10, repeat_len=10)
+
+    if "Llama-2" in model or "Mistral" in model:
+        # Llama-2 tokenizer adds an empty "" token before digits
+        reference_token_ids = [ref_tokens[1:] for ref_tokens in reference_token_ids]
+
+    # total_tokens = [
+    #     len(tokenizer.encode(prompt)) + len(completion_token_ids)
+    #     for prompt, completion_token_ids in zip(random_digit_prompts, reference_token_ids)
+    # ]
+    max_tokens = max(len(response) for response in reference_token_ids)
+
+    checkpointer.checkpoint('input_token_ids', torch.tensor(input_token_ids[0]))
+    checkpointer.checkpoint('reference_token_ids', torch.tensor(reference_token_ids[0]))
+
+    print(tokenizer.decode(input_token_ids[0]))
+    print(tokenizer.decode(reference_token_ids[0]))
+
+    topk_ll = 0
+    n_sample = 10
+    vllm_outputs = vllm_model.generate_greedy_logprobs(
+        10,
+        topk_ll,
+        prompt_token_ids=deepcopy(input_token_ids), #[deepcopy(input_token_ids[0]) for _ in range(n_sample)],
+        reference_token_ids=deepcopy(reference_token_ids), #[deepcopy(reference_token_ids)[0] for _ in range(n_sample)],
+        max_cache_tokens=checkpoint_cfg.max_cache_tokens,
+        # target_compression_rate=0.1,
+        protected_window_size=checkpoint_cfg.protected_window_size,
+        metric_collection_buffer_size=checkpoint_cfg.metric_collection_buffer_size,
+        compress_once=False,
+    )
+    del vllm_model
+    print(f"TOTAL TOKENS = {sum([len(i) + len(r) for i, r in zip(input_token_ids, reference_token_ids)])}")
+
+    def is_correct(logprobs, index):
+        max_i = None
+        max_logprob = -float('inf')
+        for i in logprobs:
+            logprob = logprobs[i].logprob
+            if logprob > max_logprob:
+                max_logprob = logprob
+                max_i = i
+        return max_i == index
+
+    for i, (ref_token_ids, (_, output, logprobs)) in enumerate(
+        zip(reference_token_ids, vllm_outputs)
+    ):
+        # assert len(logprobs) == len(ref_token_ids)
+        for i, (d, t) in enumerate(zip(logprobs, ref_token_ids)):
+            if t not in d:
+                raise Exception(f"{i}/{len(logprobs)} fail ({t} not in {d})")
+        nll = [-d[t].logprob for d, t in zip(logprobs, ref_token_ids)]
+        ppl = float("%.4f" % np.exp(sum(nll) / len(nll)))
+        acc = float("%.2f" % (sum([is_correct(d, t) for d, t in zip(logprobs, ref_token_ids)]) / len(logprobs) * 100.))
+        print(f"{acc=}, {ppl=}")
+        assert ppl < 1.1
+        assert acc > 99
+        # vllm_output = output[:len(reference_completion)]
+        # assert reference_completion == vllm_output, (
+        #     f"Test{i}:\nReference: {reference_completion!r}\nvLLM: {vllm_output!r}")
+
+
+@pytest.mark.parametrize("random_seed", [1])
+@pytest.mark.parametrize("num_digits", [100])
+@pytest.mark.parametrize("model", MODELS)
+@pytest.mark.parametrize("dtype", ["half"])
+@pytest.mark.parametrize("chunk_size", [32])  # -1 32
+# NOTE: Increasing this in this suite will fail CI because we currently cannot
+# reset distributed env properly. Use a value > 1 just when you test.
+def test_passing(
     vllm_runner,
     tokenizer_dependent_random_digit_generator,
     checkpointer,
@@ -113,13 +369,17 @@ def test_parity_with_simulated_compression(
         new_token_limit=100,
         compression_interval=1000,
         gpu_memory_utilization=0.65,
-        max_num_batched_tokens=chunk_size if chunk_size > 0 else None,
+        max_num_batched_tokens=chunk_size if chunk_size > 0 else None, # TODO test separately
         max_num_seqs=min(256, chunk_size) if chunk_size > 0 else 256,
     )
     checkpointer.set_config(checkpoint_cfg)
-
-    # Only checkpoint the first non-controlled layer
-    checkpointer.set_condition(checkpoint_layer=lambda layer: layer == 2)
+    checkpointer.do_save = True
+    checkpointer.do_validate = False
+    checkpointer.override_output = True
+    checkpointer.base_dir = './checkpoints'
+    checkpointer.tolerance = 1e-2
+    checkpointer.set_condition(checkpoint_layer=lambda x: x in CHECK_LAYERS)
+    checkpointer.enable_for_prefix("flash_prefix_decode", "flash_prefix_cache", "token_", "flash_attn__")
 
     tokenizer = AutoTokenizer.from_pretrained(model)
     input_token_ids, reference_token_ids = tokenizer_dependent_random_digit_generator(
@@ -142,12 +402,13 @@ def test_parity_with_simulated_compression(
     print(tokenizer.decode(reference_token_ids[0]))
 
     topk_ll = 0
+    n_sample = 1
     vllm_outputs = vllm_model.generate_greedy_logprobs(
         10,
         topk_ll,
-        prompt_token_ids=input_token_ids,
-        reference_token_ids=deepcopy(reference_token_ids),
-        max_cache_tokens=checkpoint_cfg.max_cache_tokens,
+        prompt_token_ids=[deepcopy(input_token_ids[0]) for _ in range(n_sample)],
+        reference_token_ids=[deepcopy(reference_token_ids)[0] for _ in range(n_sample)],
+        max_cache_tokens=-1, # checkpoint_cfg.max_cache_tokens,
         # target_compression_rate=0.1,
         protected_window_size=checkpoint_cfg.protected_window_size,
         metric_collection_buffer_size=checkpoint_cfg.metric_collection_buffer_size,
@@ -177,8 +438,8 @@ def test_parity_with_simulated_compression(
         ppl = float("%.4f" % np.exp(sum(nll) / len(nll)))
         acc = float("%.2f" % (sum([is_correct(d, t) for d, t in zip(logprobs, ref_token_ids)]) / len(logprobs) * 100.))
         print(f"{acc=}, {ppl=}")
-        assert ppl < 1.3
-        assert acc > 95
+        assert ppl < 1.1
+        assert acc > 99
         # vllm_output = output[:len(reference_completion)]
         # assert reference_completion == vllm_output, (
         #     f"Test{i}:\nReference: {reference_completion!r}\nvLLM: {vllm_output!r}")
@@ -258,7 +519,7 @@ def test_compression_with_bias(
         target_compression_rate=0.8,
         protected_window_size=100,
         metric_collection_buffer_size=10,
-        kv_head_bias_path="/irehg/vllm/kv_head_bias.npz",
+        kv_head_bias_path=MODEL_KV_HEAD_BIAS["mistralai/Mistral-7B-Instruct-v0.2"],
     )
     max_tokens = max(len(response) for response in random_digit_responses)
     tokenizer = AutoTokenizer.from_pretrained(model)

vllm/attention/backends/flash_attn.py
@@ -750,12 +750,14 @@ class FlashAttentionMetadataBuilder(
                 self.kv_metric_observation_context_lens,
                 torch.int, device,
                 self.runner.pin_memory)
-            kv_metric_observation_context_mask = torch.ones((sum(query_lens),),
-                                                            dtype=torch.bool,
-                                                            device=device)
+            kv_metric_observation_context_mask = torch.ones(
+                (sum(query_lens[:self.num_prefills]),),
+                dtype=torch.bool,
+                device=device)
             obs_end = 0
-            for l, obs_l in zip(query_lens,
-                                self.kv_metric_observation_context_lens):
+            for l, obs_l in zip(
+                query_lens[:self.num_prefills],
+                self.kv_metric_observation_context_lens[:self.num_prefills]):
                 obs_end += l
                 obs_start = obs_end - obs_l
                 kv_metric_observation_context_mask[obs_start:obs_end] = False
@@ -976,9 +978,10 @@ class FlashAttentionImpl(AttentionImpl):
         prefill_max_observed_block_size = attn_metadata.prefill_kv_metric_block_size
         obs_mask = attn_metadata.kv_metric_observation_context_mask
 
-        CHECKPOINTER.checkpoint('flash_attn__query', query)
-        CHECKPOINTER.checkpoint('flash_attn__key', key)
-        CHECKPOINTER.checkpoint('flash_attn__value', value)
+        if attn_metadata.num_decode_tokens > 0:
+            CHECKPOINTER.checkpoint('flash_attn__query', query[attn_metadata.num_prefill_tokens:], override=True)
+            CHECKPOINTER.checkpoint('flash_attn__key', key[attn_metadata.num_prefill_tokens:], override=True)
+            CHECKPOINTER.checkpoint('flash_attn__value', value[attn_metadata.num_prefill_tokens:], override=True)
 
         if kv_cache is not None:
             # Reshape the input keys and values and store them in the cache.
@@ -1024,9 +1027,13 @@ class FlashAttentionImpl(AttentionImpl):
                 # TODO how does this work with differently-sized slot mapping?
                 # if layer_index == 0:
                 #     import pdb;pdb.set_trace()
-                CHECKPOINTER.checkpoint('flash_prefix_cache_k', key[:880], max_save_iters=60)
-                CHECKPOINTER.checkpoint('flash_prefix_cache_v', value[:880], max_save_iters=60)
-                CHECKPOINTER.checkpoint('flash_prefix_slot_mapping', slot_mapping[:880], max_save_iters=60)
+                # CHECKPOINTER.checkpoint('flash_prefix_cache_k', key[:880], max_save_iters=60)
+                # CHECKPOINTER.checkpoint('flash_prefix_cache_v', value[:880], max_save_iters=60)
+                # CHECKPOINTER.checkpoint('flash_prefix_slot_mapping', slot_mapping[:880], max_save_iters=60)
+
+                if attn_metadata.num_decode_tokens > 0:
+                    CHECKPOINTER.checkpoint('flash_prefix_cache_k', key_cache, override=True)
+                    CHECKPOINTER.checkpoint('flash_prefix_cache_v', value_cache, override=True)
 
                 # key_cache_ = key_cache.clone()
                 # cache_key = key
@@ -1370,6 +1377,7 @@ class FlashAttentionImpl(AttentionImpl):
             if kvcompress_enabled:
                 # Extract layer-dependent metadata
                 block_tables = decode_meta.block_tables[layer_index]
+                slot_mapping = attn_metadata.slot_mapping[layer_index][num_prefill_tokens:]
                 # context_lens = decode_meta.context_lens_tensor[layer_index]
                 # decode_positions = attn_metadata.token_positions[num_prefill_tokens:]
                 # key_cache_, _ = KVCAttention.split_kv_cache(
@@ -1407,9 +1415,9 @@ class FlashAttentionImpl(AttentionImpl):
                 # CHECKPOINTER.checkpoint('flash_attn__decode_temp_kv_metrics', kv_metrics.temp_metrics)
 
                 ##### UPDATE
-                # if layer_index == 0:
-                #     import pdb;pdb.set_trace()
+                CHECKPOINTER.checkpoint('flash_prefix_decode__block_tables', block_tables, max_save_iters=10)
                 seq_lens_tensor = decode_meta.seq_lens_tensor[layer_index]
+                CHECKPOINTER.checkpoint('flash_prefix_decode__seq_lens', seq_lens_tensor, max_save_iters=10)
                 output[num_prefill_tokens:] = torch.ops.vllm.flash_attn_with_kvcache(
                         decode_query.unsqueeze(1),
                         key_cache,
@@ -1421,7 +1429,8 @@ class FlashAttentionImpl(AttentionImpl):
                         alibi_slopes=self.alibi_slopes,
                         softcap=self.logits_soft_cap,
                     ).squeeze(1)
-                CHECKPOINTER.checkpoint('flash_prefix_decode', output[num_prefill_tokens:])
+                CHECKPOINTER.checkpoint('flash_prefix_decode__query', decode_query, max_save_iters=10)
+                CHECKPOINTER.checkpoint('flash_prefix_decode', output[num_prefill_tokens:], max_save_iters=10)
                 ######
 
                 # assert torch.allclose(output[num_prefill_tokens:], out, rtol=1e-2, atol=1e-2)

vllm/debug.py
@@ -78,7 +78,7 @@ class Checkpointer:
             raise RuntimeError('file not found: {path}')
         return torch.load(path)
 
-    def checkpoint(self, name: str, tensor: torch.Tensor, max_save_iters: Optional[int] = None):
+    def checkpoint(self, name: str, tensor: torch.Tensor, max_save_iters: Optional[int] = None, override: bool = False):
         if max_save_iters is None:
             max_save_iters = _MAX_SAVE_ITERS
         prefix_match = any([name.startswith(prefix) for prefix in self.enabled_prefixes])
@@ -99,7 +99,8 @@ class Checkpointer:
             elif self.do_validate:
                 reference = self.torch_load(name).to(tensor.device)
                 # assert (reference.to(tensor.device) == tensor).all(), f'checkpoint {name}({self.checkpoint_counts[name]}) failed:\n{reference} != {tensor}'
-                assert torch.allclose(reference, tensor, atol=self.tolerance, rtol=self.tolerance), (
+                if not override:
+                    assert torch.allclose(reference, tensor, atol=self.tolerance, rtol=self.tolerance), (
                     f'checkpoint {name}({self.checkpoint_counts[name]}) failed:\n{reference} != {tensor}')
                 print(f'checkpoint {name}({self.checkpoint_counts[name]}) passed')
                 if self.override_output:

vllm/kvcompress/block.py
@@ -648,21 +648,21 @@ class BlockStateView:
             assert len(is_prefill) == len(self.seq_indices) == len(new_block_count)
             # Only the slot_mapping should include observation tokens.
             # Block metadata should include only tokens from the current chunk.
-            if new_obs_block_count:
+            if sum(new_obs_block_count) > 0:
                 assert len(new_obs_block_count) == len(new_block_count)
                 new_block_count = [c + c_obs for c, c_obs in zip(new_block_count, new_obs_block_count)]
             new_slot_mapping = []
             block_counts = self.get_block_counts()
             print(f'BLOCK_COUNTS: {block_counts.max()=}, {block_counts.min()=}')
             new_blocks_tensor = torch.arange(max(new_block_count), device=device)
-            for seq_idx, prefill, new_blocks in zip(self.seq_indices, is_prefill, new_block_count):
+            for i, (seq_idx, prefill, new_blocks) in enumerate(zip(self.seq_indices, is_prefill, new_block_count)):
                 if prefill:
                     # Get last new_blocks blocks along each layer and head for this sequence.
                     # Note: there may not be alignment across layers/heads since the sequence
                     # may have been compressed.
                     gather_inp = self.block_tables[:,seq_idx].transpose(1, 2)
                     gather_idx = (new_blocks_tensor[None,:new_blocks,None]
-                        + block_counts[:,seq_idx,None,:] - new_blocks)
+                        + block_counts[:,i,None,:] - new_blocks)
                     assert gather_inp.size(1) > gather_idx.max()
                     block_nums = gather_inp.gather(
                         dim=1,